a642404c140860f258c213c3002a3066cc1bd290
Sergey <nsa90202@gmail.com>
2021-12-16 08:26:24 +0200

    wrapped pages in ErrorBoundary

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,17 @@
+import { memo, useState } from "react";
 import { useProductsState } from "../../context/context";
 import CardHome from "../../components/Card/CardHome/CardHome";
 import style from "./home.module.scss";
 import LoadingBlock from "../../components/Card/CardHome/LoadingBlock";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import Spinner from "../../components/Spinner/Spinner";
-import { memo } from "react";
+import ErrorBoundary from "../../components/ErrorBoundary/ErrorBoundary";
 
 const Home = () => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemPerPage] = useState(10);
     const { process, items } = useProductsState();
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     const setContent = () => {
         const fackeArr = [...Array(20).keys()];
@@ -26,10 +30,12 @@ const Home = () => {
     const content = setContent(process, items);
 
     return (
-        <section className={style.home}>
-            <h2 className={style.home__title}>Все товары</h2>
-            <div className={style.home__wrapper}>{content}</div>
-        </section>
+        <ErrorBoundary>
+            <section className={style.home}>
+                <h2 className={style.home__title}>Все товары</h2>
+                <div className={style.home__wrapper}>{content}</div>
+            </section>
+        </ErrorBoundary>
     );
 };
 
--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -2,6 +2,7 @@ import { Link, useParams } from "react-router-dom";
 import { useProductsDispatch, useProductsState } from "../../context/context";
 import style from "./product.module.scss";
 import { memo } from "react";
+import ErrorBoundary from "../../components/ErrorBoundary/ErrorBoundary";
 
 function Product() {
     const { items } = useProductsState();
@@ -12,46 +13,48 @@ function Product() {
     const addToCart = () => dispatch({ type: "ADD_PRODUCT_TO_CART", payload: item });
 
     return (
-        <div className={style.product}>
-            <div className={style.product__promo}>
-                <img
-                    src="https://wheatskw.com/web/image/product.template/47/image_256"
-                    alt="goods"
-                />
-                <p>{item?.name}</p>
-            </div>
+        <ErrorBoundary>
+            <div className={style.product}>
+                <div className={style.product__promo}>
+                    <img
+                        src="https://wheatskw.com/web/image/product.template/47/image_256"
+                        alt="goods"
+                    />
+                    <p>{item?.name}</p>
+                </div>
 
-            <div className={style.product__info}>
-                <p className={style.product__text}>
-                    {" "}
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis
-                    enim eveniet repudiandae ullam voluptatibus! Eos incidunt, magnam
-                    minima omnis perferendis ratione sed totam velit! Atque autem
-                    consectetur magnam quod.Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit. Debitis enim eveniet repudiandae ullam
-                    voluptatibus! Eos incidunt, magnam minima omnis perferendis
-                    ratione sed totam velit! Atque autem consectetur magnam quod.
-                    Adipisci cum expedita facilis laudantium nulla placeat quasi
-                    quisquam sapiente vitae!
-                </p>
-                <p className={style.product__origin}>Made in {item?.origin}</p>
-                <div className={style.product__buy}>
-                    <div>
-                        Price: <span>{item?.price} $</span>
-                    </div>
-                    <div>
-                        <Link to="/cart">
-                            <button
-                                className={style.product__buy__button}
-                                onClick={addToCart}
-                            >
-                                Купить
-                            </button>
-                        </Link>
+                <div className={style.product__info}>
+                    <p className={style.product__text}>
+                        {" "}
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Debitis enim eveniet repudiandae ullam voluptatibus! Eos
+                        incidunt, magnam minima omnis perferendis ratione sed totam
+                        velit! Atque autem consectetur magnam quod.Lorem ipsum dolor
+                        sit amet, consectetur adipisicing elit. Debitis enim eveniet
+                        repudiandae ullam voluptatibus! Eos incidunt, magnam minima
+                        omnis perferendis ratione sed totam velit! Atque autem
+                        consectetur magnam quod. Adipisci cum expedita facilis
+                        laudantium nulla placeat quasi quisquam sapiente vitae!
+                    </p>
+                    <p className={style.product__origin}>Made in {item?.origin}</p>
+                    <div className={style.product__buy}>
+                        <div>
+                            Price: <span>{item?.price} $</span>
+                        </div>
+                        <div>
+                            <Link to="/cart">
+                                <button
+                                    className={style.product__buy__button}
+                                    onClick={addToCart}
+                                >
+                                    Купить
+                                </button>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </ErrorBoundary>
     );
 }
 
